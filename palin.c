f2652efc3483c90d464e908f7a057bfe7904163e
vaishalivsb <31504057+vaishalivsb@users.noreply.github.com>
2018-01-21 14:24:52 +0530

    Update and rename prime.c to palin.c

---
+++ palin.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a,c=0;
+scanf("%d",&a);
+for(i=1;i<=100);i++)
+{
+if(a%i==0)
+c++;  
+}
+  if(c==2)
+    printf("prime num");
+  else
+    printf("not a prime");
+} 
